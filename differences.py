a99b36a389a4a29fbc6539ca14004242eeafd8a1
Cedric <crubbertire@rogers.com>
2025-11-20 12:46:28 -0500

    Reverse String, Swap Vars, Mult Array, FizzBuzzer, and nth Fib

--- differences.py
+++ differences.py
@@ -1,5 +1,75 @@
-def getName():
-  name = input("What is your name? ")
-  return name
+## Get name from user input
+# def getName():
+#   name = input("What is your name? ")
+#   return name
+# print("Name: " + getName())
 
-print(getName())
+
+
+## Reverse name string
+# def reverseName(name):
+#   reverse = ""
+#   for index in range(len(name) -1, -1, -1): # start at max, end at 0, decrease by 1
+#     reverse += name[index]
+#   return(reverse)
+# print("Reversed Name: " + reverseName(getName()))
+
+
+
+## Swap two variables
+# def swapVariables(var1, var2):
+#   temp = var1
+#   var1 = var2
+#   var2 = temp
+
+#   return("Var1 = " + str(var1) + "\nVar2 = " + str(var2))
+# print(swapVariables(7,6))
+
+
+
+## Multiply Array Together
+# def multiplyArr(arr):
+#   if len(arr) == 0:
+#     return 1
+
+#   total = 1
+#   for num in arr:
+#     total *= num
+
+#   return total
+
+# print(multiplyArr([5,2,5]))
+
+
+
+## Fizz Buzzer
+# def fizzBuzzer(x):
+#   if x % (3 * 5) == 0:
+#     return("fizzbuzz")
+#   elif x % 3 == 0:
+#     return("fizz")
+#   elif x % 5 == 0:
+#     return("buzz")
+#   else:
+#     return("archer")
+
+# print(fizzBuzzer(5))
+
+
+
+## Nth Fibonacci
+def nthFibonacciNumber():
+  fibs = [1, 1]
+  num = int(input("Which fibonacci number do you want? "))
+  print("this")
+  if not isinstance(num, int):
+    print("Sorry, " + num + " was not a number.")
+
+  while len(fibs) < int(num):
+    length = len(fibs)
+    nextFib = fibs[length - 2] + fibs[length -1]
+    fibs.append(nextFib)
+
+  print(str(fibs[len(fibs) - 1]) + " is the fibonacci number at position " + str(num))
+
+nthFibonacciNumber()
